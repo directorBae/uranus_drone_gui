370c8b5b0f2f437a47a4cc60d2ef30b664ec5ead
Jeongwon, Bae <bjw020615@gmail.com>
2025-02-15 05:28:30 +0900

    page2 making

--- pages/vid_play/view.py
+++ pages/vid_play/view.py
@@ -1,7 +1,7 @@
 from PyQt5.QtWidgets import QWidget, QVBoxLayout, QLabel
-
 from model.store import VideoStore
-import cv2
+from ui.common.pixels import ColorGrid
+import numpy as np
 
 class VidPlayPage(QWidget):
     def __init__(self, parent=None):
@@ -15,5 +15,12 @@ class VidPlayPage(QWidget):
         # 간단한 목업 텍스트 추가
         self.label = QLabel('영상 재생 페이지', self)
         layout.addWidget(self.label)
+        
+        # 테스트 데이터를 통해 ColorGrid 추가
+        frames = [
+            np.random.randint(0, 256, (4, 4, 3), dtype=np.uint8) for _ in range(10)
+        ]
+        self.colorGrid = ColorGrid(frames)
+        layout.addWidget(self.colorGrid)
         
         self.setLayout(layout)
